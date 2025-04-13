9f000bfd5b3029b91563720b8d97a352c16393c9
Adam Carter <14096630+adammcarter@users.noreply.github.com>
2025-04-13 14:07:34 +0100

    Set makeLabel to use multiple lines

--- Tests/SnapshotsIntegrationTests/Support/makeLabel.swift
+++ Tests/SnapshotsIntegrationTests/Support/makeLabel.swift
@@ -5,6 +5,7 @@ func makeLabel(_ text: String) -> UIView {
   let label = UILabel()
   label.font = .systemFont(ofSize: UIFont.systemFontSize)
   label.text = text
+  label.numberOfLines = 0
   label.sizeToFit()
 
   return label
