b4892ed7fa4feeb44802896312e685c21c993271
Adam Carter <14096630+adamcarter93@users.noreply.github.com>
2025-09-13 16:29:31 +0100

    Updated sizes init to pass scale

--- Sources/SnapshotTestingMacros/Types/SnapshotTrait/Sizes/SizesSnapshotTrait+Length+Init.swift
+++ Sources/SnapshotTestingMacros/Types/SnapshotTrait/Sizes/SizesSnapshotTrait+Length+Init.swift
@@ -4,31 +4,37 @@ extension SnapshotTrait where Self == SizesSnapshotTrait {
   /// Allows the snapshot to render to the specific width and height.
   public static func sizes(
     width: SizesSnapshotTrait.Length,
-    height: SizesSnapshotTrait.Length
+    height: SizesSnapshotTrait.Length,
+    scale: Double = defaultScale
   ) -> Self {
     Self(
-      sizes: [.init(width: width, height: height)]
+      sizes: [
+        .init(width: width, height: height, scale: scale)
+      ]
     )
   }
 
   /// Allows the snapshot to render to the specific width and a minimum height..
   public static func sizes(
-    width: SizesSnapshotTrait.Length
+    width: SizesSnapshotTrait.Length,
+    scale: Double = defaultScale
   ) -> Self {
-    sizes(width: width, height: .minimum)
+    sizes(width: width, height: .minimum, scale: scale)
   }
 
   /// Allows the snapshot to render to the specific height and a minimum width.
   public static func sizes(
-    height: SizesSnapshotTrait.Length
+    height: SizesSnapshotTrait.Length,
+    scale: Double = defaultScale
   ) -> Self {
-    sizes(width: .minimum, height: height)
+    sizes(width: .minimum, height: height, scale: scale)
   }
 
   /// Allows the snapshot to render to the minimum possible size.
   public static func sizes(
-    _ length: SizesSnapshotTrait.Length = .minimum
+    _ length: SizesSnapshotTrait.Length = .minimum,
+    scale: Double = defaultScale
   ) -> Self {
-    sizes(width: length, height: length)
+    sizes(width: length, height: length, scale: scale)
   }
 }
