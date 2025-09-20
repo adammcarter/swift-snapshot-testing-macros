a8e9602d51f024bb476939fb4b50c54aaa0e364b
Adam Carter <14096630+adamcarter93@users.noreply.github.com>
2025-09-20 20:13:30 +0100

    Added 'imageDiff' diff tool

---
+++ Sources/SnapshotTestingMacros/Types/SnapshotTrait/DiffTool/DiffTool+DiffTools.swift
@@ -0,0 +1,13 @@
+import SnapshotTesting
+
+extension DiffToolSnapshotTrait.DiffTool {
+  public static var imageDiff: Self {
+    .init {
+      """
+      imagediff \\
+      \($0) \\
+      \($1)
+      """
+    }
+  }
+}
