a079c75e1f1bb818229979a718b850b89635c5be
Adam Carter <14096630+adamcarter93@users.noreply.github.com>
2025-09-20 20:09:45 +0100

    Removed leaking 'default' diff tool

--- Sources/SnapshotTestingMacros/Types/SnapshotTrait/DiffTool/DiffToolSnapshotTrait+Init.swift
+++ Sources/SnapshotTestingMacros/Types/SnapshotTrait/DiffTool/DiffToolSnapshotTrait+Init.swift
@@ -4,8 +4,4 @@ extension SnapshotTrait where Self == DiffToolSnapshotTrait {
   public static func diffTool(_ diffTool: DiffToolSnapshotTrait.DiffTool) -> Self {
     Self(diffTool: diffTool)
   }
-
-  public static var `default`: Self {
-    Self(diffTool: .default)
-  }
 }
